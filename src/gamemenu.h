620e546111fab800d5a86b7412d21ca05a1edfdc
REPOmAN2v2 <saniceggnog@gmail.com>
2014-08-20 16:46:40 +0200

    FUCK MENUS weird shit with menu descriptions

--- src/gamemenu.h
+++ src/gamemenu.h
@@ -2,11 +2,32 @@
 #define MENU_H_
 
 #include "include.h"
+#include <menu.h>
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+#define ENTER 	10
+#define DEFAULT	-1
+
+enum {DOWN = -1, UP = 1};
+typedef enum _counters {XC, YC, DOCC, NURC, INFC, SOLC, WOODC} Counters;
+
+typedef void (*p)(void *, int);
+typedef struct _list List;
+
+struct _list {
+	char *value;
+	List *next;
+	List *previous;
+};
 
 void displayMenu();
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
-void func(char *name);
+List * createList(char *strings[]);
+void updateUnits(int counter[]);
+void fillItems(ITEM **, int [], List *, List *);
+void listTest(WINDOW *, List *);
+void print_in_middle(WINDOW *, int, int, int, char *, chtype);
+void func(ITEM *, int);
+void toggleValue(ITEM *, int, List *, List *);
+void set_item_description (ITEM *, const char *);
 
 #endif
